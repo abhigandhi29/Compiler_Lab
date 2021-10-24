4070c3ac13b1079f9b72fabc25653fff108c4367
sajalchh <sajalchh@gmail.com>
2021-10-24 17:29:50 +0530

    changes in test1

--- Assignment5/ass5_19CS10031_19CS10051_test1.c
+++ Assignment5/ass5_19CS10031_19CS10051_test1.c
@@ -1,30 +1,36 @@
 // declarations ( variables(int, float, char), 1D array, 2D array, functions) and arithmetic operations
 
 // global declarations
-float d = 2.3;
-int s, w[10];	// 1D array declaration
-float z[2][2];	// 2D array declaration
-int a = 4, *p, b;	//pointer declaration
+float f = 2.3;	//float declaration
+int in = 5;	//int declaration
+int a[10];	// 1D array declaration
+float b[2][2];	// 2D array declaration
+int *p, b;	//pointer declaration
 void quotient(int i, float d); //function declaration		
 
 void main()
 {
 	// Variable Declaration
 	int x = 120;
-	int y = 17,i,j,k,l,m,n,o;
+	int y = 17,sum,sub,prod,div,mod,and,or;
 	char ch='c', d = 'a'; // character definitions
 
+	// Shift operations
+	y = x << 1;
+	y = x >> 1;
+
 	// Arithmetic Operations
-	i = x + y;
-	j = x - y;
-	k = x * y;
-	l = x / y;
-	m = x % y;
-	n = x & y;
-	o = x | y;
+	sum = x + y;
+	sub = x - y;
+	prod = x * y;
+	div = x / y;
+	mod = x % y;
+
+	// Bit operations
+	and = x & y;
+	or = x | y;
 	
-	y = i << 2;
-	x = i >> 1;
+	
 
 	return 0;
 }
